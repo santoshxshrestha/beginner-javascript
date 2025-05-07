f3599e5bf5f6a8a7dba763a12d1b0ef1df2d7378
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-07 16:53:06 +0545

    managed the git ignore "

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// console.log("Hello santosh")
+// if (2==2){
+//     console.log("equal")
+// }else{
+//     console.log("not equal")
+// }
+
+
+let weather = "rainny"
+if (weather== "sunny"){
+    console.log("I might get sun burn")
+}else if (weather=="foggy") {
+    console.log("I should ware some warm cloth")
+    
+}else if (weather=="rainny") {
+    console.log("I should carry an umberella")
+}else{
+    console.log("opps!! bad choice")
+}
+
+let day = prompt("Enter a day");
+switch (day) {
+    case "sunday":
+        console.log("Not again!!");
+        break;
+
+    case "Monday";
+        console.log("opps! I am already bored");
+        break;
+        
+    case "Tuesday";
+        console.log("satuarday is comming I am excited");
+        break;
+    case "Satuarday";
+        console.log("Damn it boarng")
+    default:
+        console.log("sauarday is coming");
+        break;
+}
+
