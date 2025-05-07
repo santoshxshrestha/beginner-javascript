fc200fac0faa185f2796e8d450e1593ffb9e221f
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-07 17:36:23 +0545

    added some thing

--- js/index.js
+++ js/index.js
@@ -6,35 +6,117 @@
 // }
 
 
-let weather = "rainny"
-if (weather== "sunny"){
-    console.log("I might get sun burn")
-}else if (weather=="foggy") {
-    console.log("I should ware some warm cloth")
-    
-}else if (weather=="rainny") {
-    console.log("I should carry an umberella")
-}else{
-    console.log("opps!! bad choice")
-}
+// let weather = "rainny"
+// if (weather== "sunny"){
+//     console.log("I might get sun burn")
+// }else if (weather=="foggy") {
+//     console.log("I should ware some warm cloth")
+//     
+// }else if (weather=="rainny") {
+//     console.log("I should carry an umberella")
+// }else{
+//     console.log("opps!! bad choice")
+// }
+//
+// let day = prompt("Enter a day");
+// switch (day) {
+//     case "sunday":
+//         console.log("Not again!!");
+//         break;
+//
+//     case "Monday";
+//         console.log("opps! I am already bored");
+//         break;
+//         
+//     case "Tuesday";
+//         console.log("satuarday is comming I am excited");
+//         break;
+//     case "Satuarday";
+//         console.log("Damn it boarng")
+//     default:
+//         console.log("sauarday is coming");
+//         break;
+// }
+//
 
-let day = prompt("Enter a day");
-switch (day) {
-    case "sunday":
-        console.log("Not again!!");
-        break;
 
-    case "Monday";
-        console.log("opps! I am already bored");
+// let gpa = parseInt("Enter the gpa ");
+//
+// if (gpa>= 3.6) {
+//     console.log("A+")
+// }else if (gpa>=3) {
+//     console.log("A")
+//     
+// }else if (gpa>=2.6) {
+//     console.log("B+")
+// }else if (gpa>=2) {
+//     console.log("B")
+// }else if (gpa<2) {
+//     console.log("c+")
+// }
+
+
+let num1 = parseFloat("Enter a first float value");
+let num1 = prompt(parseFloat("Enter a first float value"));
+let num2 = parseFloat("Enter a second float value");
+
+let operator = prompt("Enter the operator of the calculation");
+switch (operator) {
+    case "+":
+        console.log("num1 + num2 = ",num1+num2)
         break;
-        
-    case "Tuesday";
-        console.log("satuarday is comming I am excited");
+    case "-":
+        console.log("num1 - num2 = ",num1-num2)
         break;
-    case "Satuarday";
-        console.log("Damn it boarng")
+    case "*":
+        console.log("num1 * num2 = ",num1*num2)
+        break;
+    case "/":
+        console.log("num1/num2 = ",num1%num2)
+        break;
+    case "%":
+        console.log("num1 % num2 = ",num1%num2)
     default:
-        console.log("sauarday is coming");
+        console.log("Invald value")
         break;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
