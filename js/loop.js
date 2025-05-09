3f1601ab1ae37a6389e9018b71d1ef76cc652a5d
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-09 17:09:36 +0545

    nested loop

--- js/loop.js
+++ js/loop.js
@@ -8,7 +8,49 @@
 //     console.log(i);
 // }
 
-console.log("------------------------------")
-for(let i = 5; i> 0 ; i --,i--){
-    console.log(i);
+// console.log("------------------------------")
+// for(let i = 5; i> 0 ; i --,i--){
+//     console.log(i);
+// }
+
+// console.log("------------------------------")
+
+// for(let i = 0; i< 15 ; i++ ){
+//     console.log(i+i);
+// }
+
+// for (let i = 1; i<=10; i++){
+//
+// let emp = prompt("Enter employ");
+// let age = parseInt(prompt("Enter the age"));
+//     console.log(emp)
+//     console.log(age)
+// }
+
+
+// for(let i = 1; i<=10; i++) {
+//     console.log(`2 x `,i ,` = `, 2*i);
+// }
+
+// for(i =0; i<=4; i++){
+//     for(j=0; j<=i; j++){
+//         document.write(j)
+//     }
+//     document.write('<br>')
+// }
+
+for (let i = 1; i <= 5; i++) {
+    let line = "";
+    for (let j = 1; j <= i; j++) {
+        line += "* ";
+    }
+    console.log(line);
+}
+
+for (let i = 1; i <= 6; i++) {
+    let line = "";
+    for (let j = 1; j <= i; j++) {
+        line += "* ";
+    }
+    console.log(line);
 }
