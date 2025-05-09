135c3f4b91d1b9173dc6cd0892f9b0edcd7a5bb5
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-09 16:32:11 +0545

    basic types

---
+++ js/loop.js
@@ -0,0 +1,14 @@
+// for loop 
+
+// for (let i = 0 ; i <5; i++ ) {
+//     console.log(i);
+// }
+// console.log("------------------------------")
+// for(let i = 5; i> 0 ; i --){
+//     console.log(i);
+// }
+
+console.log("------------------------------")
+for(let i = 5; i> 0 ; i --,i--){
+    console.log(i);
+}
