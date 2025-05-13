b422e7c5e69e574aaf934eb7fbdca6b68a830677
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-13 17:08:56 +0545

    done with some of the methods in the array

--- js/array.js
+++ js/array.js
@@ -39,3 +39,20 @@ console.log(ary6)
 console.log(`this is the second element of the first array`, ary6[0][1])
 ary6[ary6.length-1] = 100
 console.log(`This is  the thing that I have added there at the last`,ary6[ary6.length-1])
+
+
+console.log([1,2,3,4,5,6,7].at(0))
+let array1 = [1,2,3,4,5,6,7,8]
+array1.push()
+console.log(`The val that I pushed `,array1[array1.length-1])
+
+//pop only removes the elements from the last index of the array
+array1.pop()
+console.log(`The val after pop `,array1[array1.length-1])
+console.log(`beore unshift`, array1)
+
+array1.unshift(23)
+
+console.log(`after unshift`, array1)
+console.log(`The val after unshift `,array1[array1.length-1])
+
