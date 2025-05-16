3ab52a812a0c7d885e490c37734295fb33136bb2
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-16 16:12:35 +0545

    soem string

---
+++ js/string_method.js
@@ -0,0 +1,31 @@
+// console.log(">hello santosh");
+//
+// console.log(`"this" is the content "here" `);
+// let names = "Santosh"
+// console.log(`"Hey there my name is ${names}"`);
+//
+//
+// console.log("this is some strin".length)
+//
+// console.log("hello".at(5))
+//
+//
+// // character checks the character ate hte given index
+// console.log("hello santosh".charAt(6));
+// let variable = "hello santosh".charAt(6);
+// console.log(typeof(variable));
+
+
+
+// console.log("hello world".charCodeAt(1))
+// let num = "32"
+// console.log(num.charCodeAt(1));
+//
+// let string = "this is some string"
+// console.log(string.indexOf("s"))
+//
+// console.log("hello world".lastIndexOf("o"))
+
+
+
+
