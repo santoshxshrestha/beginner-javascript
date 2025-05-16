41ffb57131571c98a3a4068762c0411e949975e0
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-16 17:13:35 +0545

    push

--- js/string_method.js
+++ js/string_method.js
@@ -17,76 +17,77 @@
 
 
 
-// console.log("hello world".charCodeAt(1))
-// let num = "32"
-// console.log(num.charCodeAt(1));
+// // console.log("hello world".charCodeAt(1))
+// // let num = "32"
+// // console.log(num.charCodeAt(1));
+// //
+// // let string = "this is some string"
+// // console.log(string.indexOf("s"))
+// //
+// // console.log("hello world".lastIndexOf("o"))
 //
-// let string = "this is some string"
-// console.log(string.indexOf("s"))
 //
-// console.log("hello world".lastIndexOf("o"))
-
-
-
-//pad start
 //
-console.log("hello".padStart(10,"*"));
-console.log("hello".padEnd(10,"*"));
-console.log("   hello santosh     ".trim());
-console.log("    hello santosh again    ".trimEnd());
-console.log("    hello santosh again    ".trimStart());
-
-
-console.log("hello world ".slice(0,5))
-
-
-
-
-// substrng - it is simal to sclice but it does nto accept negative values 
-
-console.log("   hello santsoh ".substring(0, 5));
-console.log("hello santosh".substring(2,0))
-console.log("hello world ".substring(-3, -7));
-
-
-//replace the content here 
-console.log("hello world".replace("hello" , "this is string"))
-
-
-console.log("-".repeat(78))
-//replace all 
-console.log("hIllo".replaceAll("I", "e"))
-
-
-//repeat the content 
-console.log("-".repeat(78))
-
-let str = "here some of the the string";
-let new_str = str.split(" ",1)
-
-
-const str12 = "hello santsoh";
-let newstr_here = str12.split(" ", 1);
-console.log(newstr_here)
-
-
-
-//includes test if the content are there in the given string 
-console.log("hello world".includes("hello santosh"));
-
-let a= "hello";
-let b= " santosh";
-
-console.log(a.concat(b))
-
-console.log("hello world".toUpperCase());
-console.log("HELLO WORLD".toLowerCase());
-console.log("hello satosh".toLocaleLowerCase());
-
+// //pad start
+// //
+// console.log("hello".padStart(10,"*"));
+// console.log("hello".padEnd(10,"*"));
+// console.log("   hello santosh     ".trim());
+// console.log("    hello santosh again    ".trimEnd());
+// console.log("    hello santosh again    ".trimStart());
+//
+//
+// console.log("hello world ".slice(0,5))
+//
+//
+//
+//
+// // substrng - it is simal to sclice but it does nto accept negative values 
+//
+// console.log("   hello santsoh ".substring(0, 5));
+// console.log("hello santosh".substring(2,0))
+// console.log("hello world ".substring(-3, -7));
+//
+//
+// //replace the content here 
+// console.log("hello world".replace("hello" , "this is string"))
+//
+//
+// console.log("-".repeat(78))
+// //replace all 
+// console.log("hIllo".replaceAll("I", "e"))
+//
+//
+// //repeat the content 
+// console.log("-".repeat(78))
+//
+// let str = "here some of the the string";
+// let new_str = str.split(" ",1)
+//
+//
+// const str12 = "hello santsoh";
+// let newstr_here = str12.split(" ", 1);
+// console.log(newstr_here)
+//
+//
+//
+// //includes test if the content are there in the given string 
+// console.log("hello world".includes("hello santosh"));
+//
+// let a= "hello";
+// let b= " santosh";
+//
+// console.log(a.concat(b))
+//
+// console.log("hello world".toUpperCase());
+// console.log("HELLO WORLD".toLowerCase());
+// console.log("hello satosh".toLocaleLowerCase());
+//
 
 //one of the use inputs there nameand yo have to convert take the first letter of the name and print as the gmail cover 
 
-let naming = prompt("Enter your name ", "Santosh");
+let naming = prompt("Enter your name ", "Santosh Shrestha");
 let firstletter = naming.split(" ")[0].charAt(0).toUpperCase();
 let lastletter = naming.split(" ")[1].charAt(0).toUpperCase();
 document.getElementById("letter").innerHTML = firstletter + lastletter;
+
