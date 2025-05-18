e40bcee260606ac464544512a5e38ae952642a8a
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-18 16:28:34 +0545

    documen write the otp

--- js/maths_form.js
+++ js/maths_form.js
@@ -10,24 +10,57 @@
 // //absolute
 // console.log(Math.abs(-12))
 // console.log(Math.pow(2,2))
-console.log(Math.pow(2,-5))
-console.log(Math.pow(-3,-3))
-console.log(Math.round(4.55))
-console.log(Math.round(4.44))
-console.log(Math.round(-4.55))
+// console.log(Math.pow(2,-5))
+// console.log(Math.pow(-3,-3))
+// console.log(Math.round(4.55))
+// console.log(Math.round(4.44))
+// console.log(Math.round(-4.55))
+//
+// console.log(Math.ceil(4.44)) //5
+// console.log(Math.ceil(4.01)) //5
+// console.log(Math.ceil(-4.21))
+// console.log(Math.ceil(-21.01))
+//
+//
+// console.log(Math.floor(-4.4))
+// console.log(Math.floor(5.9))
+//
+// console.log(Math.trunc(3.33333333333333))
+// console.log(Math.trunc(3.7777777777))
+//
+//
+// console.log(Math.max(3,4,3,53,43,53,3,3,43,52))
+// console.log(Math.min(32,23,2,332,2,32,2,32,2,52))
+// let array = [32,42,2,523,52,62,42,62,2,52,52,52]
+//
+// console.log(Math.sign(4.44))
+// console.log(Math.sign(-4.44))
+//
+// console.log(Math.log(42))
+// console.log(Math.log(10))
+// console.log(Math.sin(90) + Math.cos(90))
+//
+// console.log(Math.random());
+//
+// function generateOtp () {
+//     let digits = '0123456789';
+//     let otp = '';
+//     let len  = digits.length
+//     for (i = 0; i< 4; i++) {
+//         otp += digits[Math.floor(Math.random()*len)]
+//     }
+//     return otp;
+// }
+//
+// console.log("your otp is : ");
+// console.log(generateOtp())
 
-console.log(Math.ceil(4.44)) //5
-console.log(Math.ceil(4.01)) //5
-console.log(Math.ceil(-4.21))
-console.log(Math.ceil(-21.01))
+function generateOtp () {
+    let digits = '0123456789';
+    let otp = '';
+    let len = digits.length
+    for(i = 0; i< 4; i++)
+        otp += digits[Math.floor(Math.random()*len)]
 
-
-console.log(Math.floor(-4.4))
-console.log(Math.floor(5.9))
-
-console.log(Math.trunc(3.33333333333333))
-console.log(Math.trunc(3.7777777777))
-
-
-console.log(Math.max(3,4,3,53,43,53,3,3,43,52))
-console.log(Math.min(32,23,2,332,2,32,2,32,2,52))
+    document.getElementById("otpNum").innerHTML = otp;
+}
