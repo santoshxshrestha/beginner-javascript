cf2ca9095c4646468704ffaf7f8c2679d28ac4a5
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-20 17:05:32 +0545

    class inheritance

--- js/fnconstructor.js
+++ js/fnconstructor.js
@@ -5,40 +5,60 @@
 // the current object in person cnosdtructor are the variable 
 // name ,age, address
 
-function person(name, age, address) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
+// function person(name, age, address) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//     
+//     this.getName = function() {
+//         return `${this.name} is ${this.age} years old and lives in ${this.address} and he is ${this.nationality}`
+//     }
+// }
+//
+// const person1 = new person("santsoh", 23, "Golpark");
+// console.log(person1.getName());
+//
+// person1.name = "sanish"
+// console.log(person1.getName());
+//
+// person.prototype.nationality = "Nepali";
+// console.log(person1.getName())
+//
+// console.log(person1.nationality)
+//
+// console.log(person1.name)
+//
+// function SimpleInterest(principle, time, rate){
+//     this.principle = principle;
+//     this.time = time;
+//     this.rate = rate;
+//
+//     this.SimpleInterest = function() {
+//         let si = (this.principle * this.time * this.rate)/100
+//         return `$${si} is your total simple interest`;
+//     }
+//
+// }
+// const interest1 = new SimpleInterest(20000,4,13);
+// console.log(interest1.SimpleInterest());
+//
+class Student{
+    constructor(names, age, address, phone){
+        this.names = names;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    details() {
+        let stuentDetails = `${this.names} is ${this.age} years old and he lives in ${this.address}, his phone numer is ${this.phone}`
+        return stuentDetails;
+    }
+}
+class sectionB extends Student{
+    constructor(name, age, address, phone, rollno){
+    }
     
-    this.getName = function() {
-        return `${this.name} is ${this.age} years old and lives in ${this.address} and he is ${this.nationality}`
-    }
 }
-
-const person1 = new person("santsoh", 23, "Golpark");
-console.log(person1.getName());
-
-person1.name = "sanish"
-console.log(person1.getName());
-
-person.prototype.nationality = "Nepali";
-console.log(person1.getName())
-
-console.log(person1.nationality)
-
-console.log(person1.name)
-
-function SimpleInterest(principle, time, rate){
-    this.principle = principle;
-    this.time = time;
-    this.rate = rate;
-
-    this.SimpleInterest = function() {
-        let si = (this.principle * this.time * this.rate)/100
-        return `$${si} is your total simple interest`;
-    }
-
-}
-const interest1 = new SimpleInterest(20000,4,13);
-console.log(interest1.SimpleInterest());
-
+let santosh = new Student("Santosh", 20, "Golpark", "None of your business");
+console.log(santosh.details());
