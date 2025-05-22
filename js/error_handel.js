1176b2be91c6ebf332b4cf356b1301dcbed030a8
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-22 16:39:36 +0545

    some update no one knows wtf it is

--- js/error_handel.js
+++ js/error_handel.js
@@ -11,7 +11,7 @@ function validation() {
     let message = document.getElementById("message")
     message.innerHTML = " "
     let new_val = document.getElementById("newValue").value
-    
+
 
     try {
         if(new_val.trim() == ""){
@@ -27,4 +27,28 @@ function validation() {
         console.log("Damn it got error:", error)
     }
 }
+const numInput = () => {
+    let errMessage = document.getElementsByClassName("errMessage")[0];
+    let inputValue = document.getElementsByClassName("inputValue")[0].value;
+    console.log(inputValue);
 
+    try {
+        if (inputValue.trim() === "") {
+            throw "Hmm.. why did you skip this field!!";
+        }
+        if (isNaN(inputValue)) {
+            throw "Where is num";
+        }
+        if (Number(inputValue) < 5) {
+            throw "The number is less";
+        }
+        if (Number(inputValue) >= 20) {
+            throw "This num is more";
+        }
+        errMessage.innerHTML = "Valid input!";
+    } catch (err) {
+        errMessage.innerHTML = err;
+    } finally {
+        console.log("I am out");
+    }
+};
